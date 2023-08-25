3245eca2f0f851d86f5a19515480f446efc3dc3c
gabriellend <gdvdsn@gmail.com>
2023-08-24 22:10:33 -1000

    Add completed styles

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ const createListItem = (inputValue) => {
   const listItem = document.createElement("li");
   listItem.classList.add("main-list-item");
 
+  const item = document.createElement("label");
+  item.htmlFor = id;
+  item.textContent = inputValue;
+  item.classList.add("item");
+
   // Used for accessibility
   const hiddenCheckbox = document.createElement("input");
   hiddenCheckbox.type = "checkbox";
@@ -32,14 +37,10 @@ const createListItem = (inputValue) => {
   // Visible checkbox
   const checkbox = document.createElement("div");
   checkbox.classList.add("checkbox");
-
-  const item = document.createElement("label");
-  item.htmlFor = id;
-  item.textContent = inputValue;
-  item.classList.add("item");
+  checkbox.addEventListener("click", () => toggleComplete(item, checkbox));
 
   // Delete X icon
-  const svgString = `<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>`;
+  const svgString = `<svg class="delete-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><path fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>`;
   const svgContainer = document.createElement("div");
   svgContainer.innerHTML = svgString;
   const deleteIcon = svgContainer.querySelector(".delete-icon");
@@ -49,6 +50,18 @@ const createListItem = (inputValue) => {
   id++;
 
   return listItem;
+};
+
+const toggleComplete = (item, checkbox) => {
+  // cross out text
+  // change text color
+  // add checkmark and background gradient
+
+  item.classList.toggle("crossed-out");
+  checkbox.classList.toggle("complete");
+  const svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" stroke-width="2" d="M1 4.304L3.696 7l6-6"/></svg>`;
+  checkbox.innerHTML = svgString;
+  const checkmark = checkbox.querySelector("svg");
 };
 
 const removeItem = (listItem) => {
